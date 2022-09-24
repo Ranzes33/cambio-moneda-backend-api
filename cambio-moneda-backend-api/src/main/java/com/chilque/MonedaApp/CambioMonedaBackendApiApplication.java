94c03d6f7456f9889274a3bf824ab3a18ceef3d4
Bryan Martin Chilque <57159869+Ranzes33@users.noreply.github.com>
2022-09-23 19:10:36 -0500

    Inicio de project

---
+++ cambio-moneda-backend-api/src/main/java/com/chilque/MonedaApp/CambioMonedaBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.chilque.MonedaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambioMonedaBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambioMonedaBackendApiApplication.class, args);
+	}
+
+}
---
+++ cambio-moneda-backend-api/src/test/java/com/chilque/MonedaApp/CambioMonedaBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chilque.MonedaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioMonedaBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
